dfd631116a547cf2b54a6e06a579fef7f08d75d5
fox2049 <60031998+fox2049@users.noreply.github.com>
2025-03-13 21:48:38 +0800

    Update update_stats.py

--- .github/scripts/update_stats.py
+++ .github/scripts/update_stats.py
@@ -7,7 +7,7 @@ import random
 ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36'
 
 
-# li = """
+li = """
 <!DOCTYPE html>
 <html>
   <head>
